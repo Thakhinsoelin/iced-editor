24399cad967bc1237c1976ee27cdc4e9babe045c
soethiha <usersoe123@gmail.com>
2025-04-19 18:28:33 +0630

    final

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use std::sync::Arc;
-use iced::{font, Font, executor, Application, Command, Element, Length, Settings, Theme};
-use iced::widget::{button, column, container, horizontal_space, row, text, text_editor};
-
+use iced::{font, Font, executor, Application, Command, Element, Length, Settings, Theme, Subscription};
+use iced::widget::{pick_list, button, column, container, horizontal_space, row, text, text_editor, tooltip};
+use iced::theme;
+use iced::keyboard;
+use iced::highlighter::{self, Highlighter};
 use std::io;
 use std::path::{Path, PathBuf};
+
+//Lets see if it works
+
 fn main() -> iced::Result {
     Editor::run(Settings {
+        default_font: Font::MONOSPACE,
         fonts: vec![include_bytes!("../fonts/font/editor-icons.ttf")
             .as_slice()
             .into()],
@@ -17,7 +23,9 @@ fn main() -> iced::Result {
 struct Editor {
     path: Option<PathBuf>,
     content: text_editor::Content,
-    error: Option<Error>
+    error: Option<Error>,
+    theme: highlighter::Theme,
+    is_dirty: bool,
 }
 
 #[derive(Debug, Clone)]
@@ -28,6 +36,7 @@ enum Messages {
     FileOpened(Result<(PathBuf, Arc<String>), Error>),
     Save,
     File_Saved(Result<PathBuf, Error>),
+    Theme_Selected(highlighter::Theme),
 }
 
 impl Application for Editor {
@@ -42,6 +51,8 @@ impl Application for Editor {
                 path: None,
                 content: text_editor::Content::new(),
                 error: None,
+                theme: highlighter::Theme::SolarizedDark,
+                is_dirty: true,
             },
             Command::perform(
                 load_file(default_file()), 
@@ -57,11 +68,13 @@ impl Application for Editor {
     fn update(&mut self, message: Messages) -> Command<Messages>{
         match message {
             Messages::Edit(action) => {
-                self.content.edit(action);
+                self.is_dirty = self.is_dirty || action.is_edit();
                 self.error = None;
+                self.content.edit(action);
                 Command::none()
             }
             Messages::New => {
+                self.is_dirty = true;
                 self.path = None;
                 self.content = text_editor::Content::new();
                 Command::none()
@@ -72,6 +85,8 @@ impl Application for Editor {
             }
             Messages::File_Saved(Ok(Path)) => {
                 self.path = Some(Path);
+                self.is_dirty = false;
+
                 Command::none()
             }
             Messages::File_Saved(Err(error)) => {
@@ -82,6 +97,7 @@ impl Application for Editor {
                 Command::perform(pick_file(), Messages::FileOpened)
             }
             Messages::FileOpened(Ok((path, content))) => {
+                self.is_dirty = false;
                 self.path = Some(path);
                 self.content = text_editor::Content::with(&content); 
                 Command::none()
@@ -91,18 +107,46 @@ impl Application for Editor {
                 self.error = Some(error);
                 Command::none()
             }
+            Messages::Theme_Selected(theme) => {
+                self.theme = theme;
+                Command::none()
+            }
         }
         
     
     }
 
+    fn subscription(&self) -> Subscription<Messages> {
+        keyboard::on_key_press(|key_code, modifiers|{
+            match key_code {
+                keyboard::KeyCode::S if modifiers.command() => {
+                    Some(Messages::Save)
+                },
+                _ => None,
+            }
+        })
+    }
+
     fn view(&self) -> Element<'_, Messages> {
         let controls = row![
-            button("New").on_press(Messages::New),
-            button("Open").on_press(Messages::Open),
-            button("Save").on_press(Messages::Save),
+            action(new_icon(), "Create a New File", Some(Messages::New)),
+            action(save_icon(), "Save File", self.is_dirty.then_some(Messages::Save)),
+            action(open_icon(),"Open File", Some(Messages::Open)),
+            horizontal_space(Length::Fill),
+            pick_list(highlighter::Theme::ALL, Some(self.theme), Messages::Theme_Selected)
         ].spacing(10);
-        let input = text_editor(&self.content).on_edit(Messages::Edit);
+        let input = text_editor(&self.content)
+            .on_edit(Messages::Edit)
+            .highlight::<Highlighter>(highlighter::Settings {
+                theme: self.theme,
+                extension: self
+                    .path
+                    .as_ref()
+                    .and_then(|path| path.extension()?.to_str())
+                    .unwrap_or("rs").to_string(),
+            }, |highlight, _theme| {
+                highlight.to_format()
+            });
 
 
         let status_bar = {
@@ -128,7 +172,11 @@ impl Application for Editor {
     }
 
     fn theme(&self) -> Theme {
-        Theme::Dark
+        if self.theme.is_dark() {
+            Theme::Dark
+        } else {
+            Theme::Light
+        }
     }
 }
 
@@ -169,14 +217,42 @@ enum Error {
     IO(io::ErrorKind)
 }
 
+
+fn new_icon<'a>() -> Element<'a, Messages> {
+    icon('\u{E800}')
+}
+
+fn open_icon<'a>() -> Element<'a, Messages> {
+    icon('\u{E802}')
+}
+
+fn save_icon<'a>() -> Element<'a, Messages> {
+    icon('\u{E801}')
+}
 fn icon<'a, Message>(codepoint: char) -> Element<'a, Message>{
-    const ICON_FONT: font = Font::with_name("editor-icons");
+    const ICON_FONT: Font = Font::with_name("editor-icons");
     text(codepoint).font(ICON_FONT).into()
 }
 
-fn new_icon<'a, Message>() -> Element<'a, Messages> {
-    todo!()
+fn action<'a>(content: Element<'a, Messages>, label: &str, on_press: Option<Messages>) -> Element<'a, Messages> {
+    let is_disabled = on_press.is_none();
+    tooltip(
+        button(container(content).width(30).center_x())
+            .on_press_maybe(on_press)
+            .padding([5, 10]).style(
+            if is_disabled {
+                theme::Button::Secondary
+            } else {
+                theme::Button::Primary
+            }
+        ),
+        label,
+        tooltip::Position::FollowCursor
+    )
+        .style(theme::Container::Box)
+        .into()
 }
+
 fn default_file() -> PathBuf {
     PathBuf::from(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR")))
 }
